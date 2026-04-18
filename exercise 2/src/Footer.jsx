0a87bef90f9aaa96adb5316c93094c53fa5b34b0
Bariise11[200~ <abuukarcali2018@gmail.com>
2026-04-18 19:53:20 +0300

    updated

---
+++ exercise 2/src/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2023 Dugsiiye Blog. All rights reserved.</p>
+        </footer>
+    )
+}
+export default Footer;
---
+++ exercise 2/src/Post.jsx
@@ -0,0 +1,9 @@
+const Post = () => {
+    return (
+        <div>
+            <h2>My First Post</h2>
+            <p>This is the content of my first post.</p>
+        </div>
+    )
+}
+export default Post;
